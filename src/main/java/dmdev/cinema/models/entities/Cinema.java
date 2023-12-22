c882c4cf5745bdfd8333ff44bc3bf630b8c347be
imbaktyiar <im.baktyiar@gmail.com>
2023-12-22 19:24:34 +0600

    first commit

---
+++ src/main/java/dmdev/cinema/models/entities/Cinema.java
@@ -0,0 +1,5 @@
+package dmdev.cinema.models.entities;
+
+public class Cinema {
+    //first commit
+}
---
+++ src/test/java/dmdev/cinema/CinemaApplicationTests.java
@@ -0,0 +1,13 @@
+package dmdev.cinema;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CinemaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
